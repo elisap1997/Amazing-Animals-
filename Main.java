a066980fd6dac3ff1ea6be2fe70a8e82b66de6ef
Elisa <52206763+elisap1997@users.noreply.github.com>
2022-03-21 23:44:24 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,23 @@
+class Main {
+  public static void main(String[] args) {
+    // enter your code below this line
+    System.out.println("");
+    System.out.println("Welcome to Amazing Animals!");
+    System.out.println("");
+    System.out.println("The animal featured in this program is the African lungfish.");
+    System.out.println("");
+    System.out.print("The African lungfish lives in freshwater swamps, backwaters, and small rivers in West and South Africa.");
+    System.out.println(" These prehistoric animals survived unchanged for " + 400 + " million years.");
+    System.out.println("");
+    System.out.println("Did you know that the African lungfish has two lungs and can breathe air?");
+    System.out.println("");
+    System.out.print("This is because the African lungfish lives in flood plains in waterways that dry up.");
+    System.out.println("To manage this, the African lungfish secretes a thin layer of mucus around itself that dries into a cocoon. It can live in the cocoon for up to 1 year, or " + (1*12) + " months.");
+    System.out.println("");
+    System.out.print("The African lungfish also hibernates in water. It digs a hole of 1-9 inches and can remain there for up to ");
+    System.out.print(4);
+    System.out.println(" years.");
+    System.out.println("");
+    System.out.println("Hope you enjoyed this short presentation about the African lungfish!");
+  }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+Hello!
+
+Amazing Animals is a project I did in Java. This program displays facts and figures about the African lungfish.
+
+In the code, you'll notice that I have incorporated both text and numeric values. In addition, I have also concatenated strings. 
+
